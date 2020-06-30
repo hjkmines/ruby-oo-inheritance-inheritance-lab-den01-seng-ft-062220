20dd32e912da59afdbfa6a05d3c815d903aece71
Hyung Kim <hjkmines@gmail.com>
2020-06-30 23:11:03 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,16 +1,16 @@
 class User 
   
   attr_accessor :first_name, :last_name
-  @@all = []
+  # @@all = []
   
-  def initialize(first_name, last_name) 
-    @first_name = first_name
-    @last_name = last_name
-    @@all << self 
-  end 
+  # def initialize(first_name, last_name) 
+  #   @first_name = first_name
+  #   @last_name = last_name
+  #   @@all << self 
+  # end 
   
-  def self.all 
-    @@all 
-  end 
+  # def self.all 
+  #   @@all 
+  # end 
   
 end 
